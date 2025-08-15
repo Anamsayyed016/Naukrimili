fc5860f7912aa59197e49c89164e1befd4084889
anamsayyed016 <anamsayyed58@gmail.com>
2025-08-15 18:22:03 +0530

    Fix webpack compilation errors in ResumeUpload component and toast types

--- types/shims.d.ts
+++ types/shims.d.ts
@@ -2,7 +2,22 @@
 // type packages are not present. These should be removed once the
 // corresponding @types or libraries are properly installed.
 
-declare module 'react';
+declare module 'react' {
+  export = React;
+  export as namespace React;
+  
+  namespace React {
+    type ReactNode = any;
+    type ReactElement = any;
+    type ComponentType<P = any> = any;
+    type FC<P = {}> = any;
+    
+    function useState<T>(initialState: T | (() => T)): [T, (newState: T | ((prevState: T) => T)) => void];
+    function useEffect(effect: () => void | (() => void), deps?: any[]): void;
+    function forwardRef<T, P = {}>(render: (props: P, ref: React.Ref<T>) => React.ReactElement | null): React.ComponentType<P & { ref?: React.Ref<T> }>;
+  }
+}
+
 declare module 'react-dom';
 declare module 'react-dropzone';
 declare module 'lucide-react';
