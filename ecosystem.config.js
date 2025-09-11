88a93608520f4b8a51fbb8cb8d608fd94b9fbd28
anamsayyed016 <anamsayyed58@gmail.com>
2025-09-11 18:43:01 +0530

    server

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+export default {
+  apps: [{
+    name: 'jobportal',
+    script: 'npm',
+    args: 'run dev',
+    cwd: './',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development',
+      PORT: 3000
+    },
+    env_production: {
+      NODE_ENV: 'production',
+      PORT: 3000
+    }
+  }]
+};
