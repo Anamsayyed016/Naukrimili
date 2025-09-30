bd245407fedb48a4fbfd0304ad843d81495878d5
anamsayyed016 <anamsayyed58@gmail.com>
2025-09-30 17:35:23 +0530

    serverll

--- app/jobs/page.tsx
+++ app/jobs/page.tsx
@@ -13,7 +13,7 @@ export default function JobsPage() {
         <EnhancedJobSearchHero
           showHistory={true}
           showSuggestions={true}
-          showAdvancedFilters={true}
+          showAdvancedFilters={false}
         />
       </div>
 
