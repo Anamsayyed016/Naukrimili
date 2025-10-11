744f776c0000cc07c864b8446c1d2b38794c18f6
anamsayyed016 <anamsayyed58@gmail.com>
2025-10-11 15:01:53 +0530

    Simplify layout to fix React error 310 - remove problematic components

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,12 +5,7 @@ import './globals.css';
 import MainNavigation from '@/components/MainNavigation';
 import Footer from '@/components/Footer';
 import SessionProvider from '@/components/SessionProvider';
-import BufferPolyfill from '@/components/BufferPolyfill';
 import { Toaster } from '@/components/ui/toaster';
-import GlobalErrorHandler from '@/components/GlobalErrorHandler';
-import ForceNewHash from '@/components/ForceNewHash';
-import CSSLoader from '@/components/CSSLoader';
-// FORCE HASH CHANGE - Build timestamp: 2025-10-02 14:30:00 - MAJOR CHANGE
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -28,10 +23,6 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} font-body`}>
         <SessionProvider>
-          <GlobalErrorHandler />
-          <BufferPolyfill />
-          <ForceNewHash />
-          <CSSLoader />
           <MainNavigation />
           {children}
           <Footer />
