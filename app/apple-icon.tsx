7cc3791a366c61be7895aa34ae70013c91be4c72
anamsayyed016 <anamsayyed58@gmail.com>
2025-11-07 01:45:04 +0530

    serverll

--- app/apple-icon.tsx
+++ app/apple-icon.tsx
@@ -1,14 +1,27 @@
-export const runtime = 'edge'
-
+import { ImageResponse } from 'next/og'
+ 
+// Image metadata
 export const size = {
   width: 180,
   height: 180,
 }
 export const contentType = 'image/png'
-
-export default async function AppleIcon() {
-  // Fetch the Cloudinary image
-  const imageUrl = 'https://res.cloudinary.com/dko2hk0yo/image/upload/e_make_transparent:10,w_180,h_180,c_fit/v1762455309/nukrimilogo2_tybkqx.png'
-  
-  return fetch(imageUrl).then(res => res)
+ 
+// Image generation
+export default function AppleIcon() {
+  return new ImageResponse(
+    (
+      // eslint-disable-next-line @next/next/no-img-element
+      <img
+        src="https://res.cloudinary.com/dko2hk0yo/image/upload/e_make_transparent:10,w_180,h_180,c_fit/v1762455309/nukrimilogo2_tybkqx.png"
+        alt="NaukriMili"
+        width={180}
+        height={180}
+      />
+    ),
+    {
+      ...size,
+    }
+  )
+}
 
--- app/icon.tsx
+++ app/icon.tsx
@@ -1,13 +1,26 @@
-export const runtime = 'edge'
-
+import { ImageResponse } from 'next/og'
+ 
+// Image metadata
 export const size = {
   width: 32,
   height: 32,
 }
 export const contentType = 'image/png'
-
-export default async function Icon() {
-  // Fetch the Cloudinary image
-  const imageUrl = 'https://res.cloudinary.com/dko2hk0yo/image/upload/e_make_transparent:10,w_32,h_32,c_fit/v1762455309/nukrimilogo2_tybkqx.png'
-  
-  return fetch(imageUrl).then(res => res)
+ 
+// Image generation
+export default function Icon() {
+  return new ImageResponse(
+    (
+      // eslint-disable-next-line @next/next/no-img-element
+      <img
+        src="https://res.cloudinary.com/dko2hk0yo/image/upload/e_make_transparent:10,w_32,h_32,c_fit/v1762455309/nukrimilogo2_tybkqx.png"
+        alt="NaukriMili"
+        width={32}
+        height={32}
+      />
+    ),
+    {
+      ...size,
+    }
+  )
+}
