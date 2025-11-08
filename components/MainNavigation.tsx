4fe0f407fdcf2cdc525a60c48bf3cccf4a848ae8
anamsayyed016 <anamsayyed58@gmail.com>
2025-11-08 16:09:05 +0530

    serverll

--- components/MainNavigation.tsx
+++ components/MainNavigation.tsx
@@ -96,14 +96,14 @@ export default function MainNavigation({
           <div className="flex items-center justify-between h-16 lg:h-20">
             <Link href="/" className="flex items-center hover:opacity-90 transition-opacity duration-300">
               <Image 
-                src="https://res.cloudinary.com/dko2hk0yo/image/upload/e_replace_color:lightgray:80:white,w_320,h_80,c_fit,f_auto,q_auto/v1762546509/1naukkkLogo_gw9g5z.jpg" 
+                src="https://res.cloudinary.com/dko2hk0yo/image/upload/e_replace_color:C0C0C0:100:FFFFFF/e_replace_color:D3D3D3:100:FFFFFF/e_replace_color:E8E8E8:100:FFFFFF/b_white,c_pad,w_340,h_85,f_png,q_auto/v1762546509/1naukkkLogo_gw9g5z.jpg" 
                 alt="NaukriMili - Job Portal" 
-                className="h-[52px] sm:h-[60px] lg:h-[68px] w-auto object-contain max-w-[170px] sm:max-w-[210px] lg:max-w-[250px]"
-                width={320}
-                height={80}
+                className="h-[56px] sm:h-[64px] lg:h-[72px] w-auto object-contain max-w-[180px] sm:max-w-[220px] lg:max-w-[260px]"
+                width={340}
+                height={85}
                 priority
                 unoptimized
-                style={{ display: 'block', imageRendering: 'crisp-edges' }}
+                style={{ display: 'block' }}
               />
             </Link>
             <div className="w-8 h-8 animate-pulse bg-gray-200 rounded"></div>
@@ -171,14 +171,14 @@ export default function MainNavigation({
           {/* Brand Logo */}
           <Link href="/" className="flex items-center hover:opacity-90 transition-opacity duration-300">
             <Image 
-              src="https://res.cloudinary.com/dko2hk0yo/image/upload/e_replace_color:lightgray:80:white,w_320,h_80,c_fit,f_auto,q_auto/v1762546509/1naukkkLogo_gw9g5z.jpg" 
+              src="https://res.cloudinary.com/dko2hk0yo/image/upload/e_replace_color:C0C0C0:100:FFFFFF/e_replace_color:D3D3D3:100:FFFFFF/e_replace_color:E8E8E8:100:FFFFFF/b_white,c_pad,w_340,h_85,f_png,q_auto/v1762546509/1naukkkLogo_gw9g5z.jpg" 
               alt="NaukriMili - Job Portal" 
-              className="h-[52px] sm:h-[60px] lg:h-[68px] w-auto object-contain max-w-[170px] sm:max-w-[210px] lg:max-w-[250px]"
-              width={320}
-              height={80}
+              className="h-[56px] sm:h-[64px] lg:h-[72px] w-auto object-contain max-w-[180px] sm:max-w-[220px] lg:max-w-[260px]"
+              width={340}
+              height={85}
               priority
               unoptimized
-              style={{ display: 'block', imageRendering: 'crisp-edges' }}
+              style={{ display: 'block' }}
             />
           </Link>
 
