b1338753fe5c8529a3fe7b5c9099339ffbdf18a8
anamsayyed016 <anamsayyed58@gmail.com>
2025-11-09 01:55:35 +0530

    feat: optimize logo size to 95% navbar fill for maximum visibility

--- components/MainNavigation.tsx
+++ components/MainNavigation.tsx
@@ -98,7 +98,7 @@ export default function MainNavigation({
               <Image 
                 src="https://res.cloudinary.com/dko2hk0yo/image/upload/e_bgremoval/f_png/q_auto/v1762626132/naulogokriil1_aqjojr.png" 
                 alt="NaukriMili - Job Portal" 
-                className="h-[72px] sm:h-[78px] lg:h-[88px] w-auto object-contain"
+                className="h-[76px] sm:h-[78px] lg:h-[92px] w-auto object-contain"
                 width={400}
                 height={160}
                 priority
